b1d035271ebd4ee15275844e0e5a25925496307a
fkunkel <fkunkel@wombatsecurity.com>
2016-07-18 14:17:32 -0400

    omment out config command

--- spring-session/src/main/java/org/springframework/session/data/redis/config/ConfigureNotifyKeyspaceEventsAction.java
+++ spring-session/src/main/java/org/springframework/session/data/redis/config/ConfigureNotifyKeyspaceEventsAction.java
@@ -52,21 +52,21 @@ public class ConfigureNotifyKeyspaceEventsAction implements ConfigureRedisAction
 	 * springframework.data.redis.connection.RedisConnection)
 	 */
 	public void configure(RedisConnection connection) {
-		String notifyOptions = getNotifyOptions(connection);
-		String customizedNotifyOptions = notifyOptions;
-		if (!customizedNotifyOptions.contains("E")) {
-			customizedNotifyOptions += "E";
-		}
-		boolean A = customizedNotifyOptions.contains("A");
-		if (!(A || customizedNotifyOptions.contains("g"))) {
-			customizedNotifyOptions += "g";
-		}
-		if (!(A || customizedNotifyOptions.contains("x"))) {
-			customizedNotifyOptions += "x";
-		}
-		if (!notifyOptions.equals(customizedNotifyOptions)) {
-			connection.setConfig(CONFIG_NOTIFY_KEYSPACE_EVENTS, customizedNotifyOptions);
-		}
+//		String notifyOptions = getNotifyOptions(connection);
+//		String customizedNotifyOptions = notifyOptions;
+//		if (!customizedNotifyOptions.contains("E")) {
+//			customizedNotifyOptions += "E";
+//		}
+//		boolean A = customizedNotifyOptions.contains("A");
+//		if (!(A || customizedNotifyOptions.contains("g"))) {
+//			customizedNotifyOptions += "g";
+//		}
+//		if (!(A || customizedNotifyOptions.contains("x"))) {
+//			customizedNotifyOptions += "x";
+//		}
+//		if (!notifyOptions.equals(customizedNotifyOptions)) {
+//			connection.setConfig(CONFIG_NOTIFY_KEYSPACE_EVENTS, customizedNotifyOptions);
+//		}
 	}
 
 	private String getNotifyOptions(RedisConnection connection) {
